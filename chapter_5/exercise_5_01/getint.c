d686837efc42e3ac33cd383ed8d8ec63929ee226
xy gu <2793392818@qq.com>
2024-02-11 14:09:54 +0800

    add exercise 5-1

--- chapter_5/exercise_5_01/getint.c
+++ chapter_5/exercise_5_01/getint.c
@@ -1,105 +1,97 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 #define MAXLEN 1000
 #define BUFFSIZE 100
 
 void printbuf(void);
+
 int getch(void);
+
 void ungetch(int c);
+
 int getint(int *pn);
 
-int main(void)
-{
-  int number = 0;
+int main(void) {
+    int number = 0;
 
-  getint(&number);
-  printf("%d\n", number);
+    int ret = getint(&number);
+    printf("get:%d, return val:%c\n", number, ret);
 
-  printbuf();
+    printbuf();
 
-  return 0;
+    return 0;
 }
 
+// `bufp` is the index of the next free position in buf
 int bufp = 0;
 int buf[BUFFSIZE];
 
-void printbuf(void)
-{
-  if (bufp)
-  {
-    printf("Buffer: [ ");
+void printbuf(void) {
+    if (bufp) {
+        printf("Buffer: [ ");
 
-    int i;
-    for (i = bufp - 1; i >= 0; --i)
-    {
-      if (i)
-      {
-        printf("'%c', ", buf[i] != '\n' ? buf[i] : '.');
-      }
-      else
-      {
-        printf("'%c' ", buf[i] != '\n' ? buf[i] : '.');
-      }
+        int i;
+        for (i = bufp - 1; i >= 0; --i) {
+            if (i) {
+                printf("'%c', ", buf[i] != '\n' ? buf[i] : '.');
+            } else {
+                // i == 0, the last element, so no comma after it
+                printf("'%c' ", buf[i] != '\n' ? buf[i] : '.');
+            }
+        }
+
+        printf("]\n");
+    }
+}
+
+int getch(void) {
+    return bufp > 0 ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= BUFFSIZE) {
+        printf("ungetch: too many characters\n");
+    } else {
+        buf[bufp++] = c;
+    }
+}
+
+/**
+ * getint: get next integer from input into *pn
+ * @param pn: Pointer to an integer where the input integer will be stored.
+ * @return: The last character read from the input.
+ */
+int getint(int *pn) {
+    int c, sign;
+
+    while (isspace(c = getch()));
+
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);
+        return 0;
     }
 
-    printf("]\n");
-  }
-}
+    sign = (c == '-') ? -1 : 1;
 
-int getch(void)
-{
-  return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c)
-{
-  if (bufp >= BUFFSIZE)
-  {
-    printf("ungetch: too many characters\n");
-  }
-  else
-  {
-    buf[bufp++] = c;
-  }
-}
-
-int getint(int *pn)
-{
-  int c, sign;
-
-  while (isspace(c = getch()))
-    ;
-
-  if (!isdigit(c) && c != EOF && c != '+' && c != '-')
-  {
-    ungetch(c);
-    return 0;
-  }
-
-  sign = (c == '-') ? -1 : 1;
-
-  if (c == '+' || c == '-')
-  {
-    if (!isdigit(c = getch()))
-    {
-      ungetch(c);
-      ungetch(sign == 1 ? '+' : '-');
-      return 0;
+    if (c == '+' || c == '-') {
+        if (!isdigit(c = getch())) {
+            ungetch(c);
+            ungetch(sign == 1 ? '+' : '-');
+            return 0;
+        }
     }
-  }
 
-  for (*pn = 0; isdigit(c); c = getch())
-  {
-    *pn = 10 * *pn + (c - '0');
-  }
+    for (*pn = 0; isdigit(c); c = getch()) {
+        *pn = 10 * *pn + (c - '0');
+    }
 
-  *pn = *pn * sign;
+    *pn = *pn * sign;
 
-  if (c != EOF)
-  {
-    ungetch(c);
-  }
+    // `c` is the last character read from the input, and it's not a digit
+    if (c != EOF) {
+        ungetch(c);
+    }
 
-  return c;
+    return c;
 }
