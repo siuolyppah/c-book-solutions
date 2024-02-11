bb80fd4b9d5c36df03b0e4e45c9171f057223a44
xy gu <2793392818@qq.com>
2024-02-11 14:45:17 +0800

    add exercise 5-3

--- chapter_5/exercise_5_03/strcat.c
+++ chapter_5/exercise_5_03/strcat.c
@@ -2,26 +2,24 @@
 
 void strcat_ptr(char *s, char *t);
 
-int main(void)
-{
-  char s[100] = "This is the first string";
-  char t[] = ", this second string!";
+int main(void) {
+    char s[100] = "This is the first string";
+    char t[] = ", this second string!";
 
-  strcat_ptr(s, t);
+    strcat_ptr(s, t);
 
-  puts(s);
+    puts(s);
 
-  return 0;
+    return 0;
 }
 
-// Concatenate t to end of s; s must be big enough.
-void strcat_ptr(char *s, char *t)
-{
-  // Find the end of s
-  while ((*++s) != '\0')
-    ;
+// concatenate t to end of s; s must be big enough
+void strcat_ptr(char *s, char *t) {
+    // stop when *s == '\0'
+    while (*s != '\0') {
+        ++s;
+    }
 
-  // copy t to the end of s
-  while ((*s++ = *t++) != '\0')
-    ;
+    // copy t to the end of s
+    while ((*s++ = *t++) != '\0');
 }
